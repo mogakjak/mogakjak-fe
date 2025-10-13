5a8299fc49798e6c00af408b3ad0209274d4265e
betteree <kkne1017@gmail.com>
2025-10-14 00:07:44 +0900

    feat: 로그인 페이지 ui

--- src/app/(pages)/login/components/loginButton.tsx
+++ src/app/(pages)/login/components/loginButton.tsx
@@ -4,21 +4,21 @@ import { LoginButtonProps } from "../_types/login";
 export default function LoginButton({ type, onClick }: LoginButtonProps) {
   const config = {
     google: {
-      text: "text-black",
+      style: " border-gray-200 bg-white ",
       label: "구글 로그인/회원가입",
-      icon: "/icons/google.svg",
+      icon: "/Icons/google.svg",
     },
     kakao: {
-      text: "text-black",
+      style: "border-[#FADD0E] bg-[#FADD0E] ",
       label: "카카오 로그인/회원가입",
-      icon: "/icons/kakao.svg",
+      icon: "/Icons/kakao.svg",
     },
   }[type];
 
   return (
     <button
       onClick={onClick}
-      className={`w-[360px] flex items-center justify-center gap-2 rounded-lg border px-10 py-4`}
+      className={`bodey1_16M w-[360px] rounded-2xl flex items-center justify-center gap-2 border px-10 py-4 ${config.style}`}
     >
       <Image src={config.icon} alt={type} width={20} height={20} />
       <span>{config.label}</span>
