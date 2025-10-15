f7bc5121676d6ae7828837c73275041e1152f078
betteree <kkne1017@gmail.com>
2025-10-15 23:32:32 +0900

    style: 디자인 토큰 세팅 변경

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{ts,tsx,js,jsx,mdx}",
+    "./pages/**/*.{ts,tsx,js,jsx,mdx}",
+    "./components/**/*.{ts,tsx,js,jsx,mdx}",
+    "./src/**/*.{ts,tsx,js,jsx,mdx}",
+  ],
+};
