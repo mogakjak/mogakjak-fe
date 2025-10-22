0f91a418987bb601d5400469d0cda4936d156762
betteree <kkne1017@gmail.com>
2025-10-22 11:14:08 +0900

    feat: 메뉴 ui

--- src/app/(pages)/mypage/_components/menu/menuMain.tsx
+++ src/app/(pages)/mypage/_components/menu/menuMain.tsx
@@ -1,5 +1,27 @@
+"use client";
+
+import { useState } from "react";
+import MenuTab from "./menuTab";
+
 export default function Menu() {
+  const [selected, setSelected] = useState("내 채소 바구니");
+
+  const menuItems = ["내 채소 바구니", "내 모각작 메이트"];
+
   return (
-    <div className="w-[327px] h-[212px] p-6 rounded-[20px] bg-white">메뉴</div>
+    <div className="w-[327px] p-6 rounded-[20px] bg-white">
+      <h2 className="text-heading4-20SB text-black mb-7">MENU</h2>
+
+      <div className="flex flex-col gap-3">
+        {menuItems.map((item) => (
+          <MenuTab
+            key={item}
+            title={item}
+            selected={selected === item}
+            onClick={() => setSelected(item)}
+          />
+        ))}
+      </div>
+    </div>
   );
 }
