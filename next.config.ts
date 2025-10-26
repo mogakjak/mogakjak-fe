3ffd3a84782bb89b0f17c56fe38408bdf9b52637
betteree <kkne1017@gmail.com>
2025-10-26 23:46:36 +0900

    feat: 그룹 타이머 설정 및 svg 컴포넌트 설정

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,48 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    const assetRule = (config.module.rules as any[]).find(
+      (rule) => rule?.test?.test && rule.test.test(".svg")
+    );
+    if (assetRule) {
+      assetRule.exclude = /\.svg$/i;
+    }
+
+    config.module.rules.push({
+      test: /\.svg$/i,
+      oneOf: [
+        { resourceQuery: /url/, type: "asset/resource" },
+
+        {
+          issuer: /\.[jt]sx?$/,
+          use: [
+            {
+              loader: "@svgr/webpack",
+              options: {
+                svgo: true,
+                svgoConfig: {
+                  plugins: [
+                    { name: "removeAttrs", params: { attrs: "(fill|stroke)" } },
+                  ],
+                },
+                typescript: true,
+                expandProps: "end",
+              },
+            },
+          ],
+        },
+      ],
+    });
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "/Icons": path.resolve(process.cwd(), "public/Icons"),
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
