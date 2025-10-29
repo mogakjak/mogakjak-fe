630ec9d8df80a08a98d680caaa75e4c1a98b0547
betteree <kkne1017@gmail.com>
2025-10-29 23:41:11 +0900

    feat: 컴포넌트 분리 및 모달 클릭 방지

--- src/app/_components/group/sidebar/groupSidebar.tsx
+++ src/app/_components/group/sidebar/groupSidebar.tsx
@@ -8,13 +8,11 @@ import SidebarButton from "./sidebarButton";
 
 // 아이콘
 import Add from "/Icons/add.svg";
-import Edit from "/Icons/edit.svg";
 import Notification from "/Icons/notification.svg";
-import GoalModal from "../modal/goalModal";
 import NotiModal from "../modal/notiModal";
+import GroupGoal from "./groupGoal";
 
 export default function GroupSidebar() {
-  const [openGoal, setOpenGoal] = useState(false);
   const [openNoti, setOpenNoti] = useState(false);
   return (
     <div className="min-h-screen bg-white p-9 min-w-[343px]">
@@ -32,25 +30,7 @@ export default function GroupSidebar() {
 
       <GroupMembers />
 
-      <div className="flex flex-col mt-5 py-5 border-t border-b border-gray-200">
-        <div className="flex items-center justify-between w-full">
-          <h3 className="text-heading4-20SB text-black">그룹 공동 목표</h3>
-          <button onClick={() => setOpenGoal(true)}>
-            <Icon Svg={Edit} size={24} className="text-gray-600" />
-          </button>
-        </div>
-
-        <div className="flex gap-10 items-center justify-center mt-[27px]">
-          <div className="flex flex-col items-center">
-            <p className="text-body2-14R text-gray-600">목표 시간</p>
-            <p className="text-heading3-24SB text-gray-800">0h 00m</p>
-          </div>
-          <div className="flex flex-col items-center">
-            <p className="text-body2-14R text-gray-600">달성률</p>
-            <p className="text-heading3-24SB text-red-500">0%</p>
-          </div>
-        </div>
-      </div>
+      <GroupGoal></GroupGoal>
 
       <div className="flex flex-col gap-3 mt-10">
         <h3 className="text-heading4-20SB text-black">그룹 공동 타이머</h3>
@@ -61,21 +41,14 @@ export default function GroupSidebar() {
         <GroupTimer />
       </div>
 
-      {openGoal && (
-        <div
-          className="fixed inset-0 bg-black/30 flex items-center justify-center z-50"
-          onClick={() => setOpenGoal(false)}
-        >
-          <GoalModal onClose={() => setOpenGoal(false)} />
-        </div>
-      )}
-
       {openNoti && (
         <div
           className="fixed inset-0 bg-black/30 flex items-center justify-center z-50"
           onClick={() => setOpenNoti(false)}
         >
-          <NotiModal onClose={() => setOpenNoti(false)} />
+          <div className="relative" onClick={(e) => e.stopPropagation()}>
+            <NotiModal onClose={() => setOpenNoti(false)} />
+          </div>
         </div>
       )}
     </div>
