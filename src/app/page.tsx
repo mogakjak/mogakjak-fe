97b33453dbcda3c956dc08fc4e13d31e6a4449b2
betteree <kkne1017@gmail.com>
2025-10-30 21:25:31 +0900

    style: 높이 맞추기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import RoomMain from "./_components/home/roomMain";
 
 export default function Home() {
   return (
-    <main className="w-full max-w-[1440px] min-h-screen py-9 mx-auto flex gap-5 items-center overflow-x-hidden">
+    <main className="w-full max-w-[1440px] min-h-screen py-9 mx-auto flex gap-5 items-stretch overflow-x-hidden">
       <PreviewMain />
 
-      <section className="w-full flex flex-col gap-5">
+      <section className="w-full flex flex-col self-stretch gap-5">
         <RoomMain />
         <FriendMain />
       </section>
