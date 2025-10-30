ebbfef150703b140e33cdb6a5cfef545d817f64e
betteree <kkne1017@gmail.com>
2025-10-30 22:59:20 +0900

    style: 헤더 1440px 픽스

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,14 +4,16 @@ import ProfileButton from "./header/ProfileButton";
 
 export default function Header() {
   return (
-    <header className="relative w-full flex justify-between px-9 py-4 bg-white border-b border-gray-200">
-      <Image src="/logo.svg" alt="logo" width={105} height={36}></Image>
-      <nav className="flex gap-3">
-        <HeaderButton text="Home" href="/" />
-        <HeaderButton text="TO DO" href="/todo" />
-        <HeaderButton text="기록" href="/record" />
-        <ProfileButton />
-      </nav>
+    <header className="relative w-full flex items-center justify-center bg-white border-b border-gray-200">
+      <div className="w-[1440px] flex justify-between items-center px-9 py-4">
+        <Image src="/logo.svg" alt="logo" width={105} height={36}></Image>
+        <nav className="flex gap-3">
+          <HeaderButton text="Home" href="/" />
+          <HeaderButton text="TO DO" href="/todo" />
+          <HeaderButton text="기록" href="/record" />
+          <ProfileButton />
+        </nav>
+      </div>
     </header>
   );
 }
