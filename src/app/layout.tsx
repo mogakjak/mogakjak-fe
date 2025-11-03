c2963e8661ba5ed526f67e8b8902befe01847124
betteree <kkne1017@gmail.com>
2025-11-03 20:03:13 +0900

    feat: 썸네일 및 메타태그 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,23 @@ import ConditionalHeader from "@/components/header/ConditionalHeader";
 
 export const metadata: Metadata = {
   title: "모각작",
-  description: "모각작",
+  description: "함께 몰입하며 꾸준함을 만드는 모각작 커뮤니티",
+  openGraph: {
+    title: "김이름님이 `몰딥브 팀` 으로 초대했어요!",
+    description: "타이머로 함께 몰입하며 꾸준함을 만드는 힘을 경험해 보세요!",
+    url: "https://mogakjak-fe.vercel.app",
+    siteName: "모각작",
+    images: [
+      {
+        url: "https://mogakjak-fe.vercel.app/thumbnail.png",
+        width: 1200,
+        height: 630,
+        alt: "모각작 초대",
+      },
+    ],
+    locale: "ko_KR",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
