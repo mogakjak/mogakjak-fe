a8959655e6c58e27497a0049a4215eef1b1d6668
betteree <kkne1017@gmail.com>
2025-11-19 15:37:43 +0900

    fix: 빌드 오류 수정

--- src/app/(pages)/group/[groupid]/page.tsx
+++ src/app/(pages)/group/[groupid]/page.tsx
@@ -1,5 +1,11 @@
 import GroupRoomPage from "../_components/groupRoomPage";
 
-export default function Page({ params }: { params: { groupId: string } }) {
-  return <GroupRoomPage groupId={params.groupId} />;
+type GroupPageProps = {
+  params: Promise<{ groupId: string }>;
+};
+
+export default async function Page({ params }: GroupPageProps) {
+  const { groupId } = await params;
+
+  return <GroupRoomPage groupId={groupId} />;
 }
