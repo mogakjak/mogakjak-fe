d40ce2d1441a4cd86d92a037d77d3fe5ea487d8b
psm1st <psm1st@sookmyung.ac.kr>
2026-02-09 17:19:14 +0900

    fix: 로그인 페이지 탈퇴한 사람의 리다이렉트 실패로 클라이언트 컴포넌트 제작

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -1,23 +1,5 @@
-import Image from "next/image";
-import LoginButton from "./components/loginButton";
+import LoginPageClient from "./components/LoginPageClient";
 
 export default function Login() {
-  return (
-    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100">
-      <Image
-        src="/Icons/logoSlogan.svg"
-        alt="slogan"
-        width={257}
-        height={224}
-      ></Image>
-      <p className="text-body1-16R mt-5 text-gray-600">
-        함께 몰입하며 꾸준함을 만드는 힘을 경험해 보세요!
-      </p>
-
-      <div className="mt-20 gap-4 flex flex-col">
-        <LoginButton type="google"></LoginButton>
-        <LoginButton type="kakao"></LoginButton>
-      </div>
-    </div>
-  );
+  return <LoginPageClient />;
 }
