3052656ae790cfc7dab9e2f9bb7c3f01063d62c2
betteree <kkne1017@gmail.com>
2026-03-03 16:43:12 +0900

    feat:  clarity 입력

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { GoogleAnalytics } from "@next/third-parties/google"
+import Script from "next/script";
 import "./globals.css";
 import dynamic from "next/dynamic";
 
@@ -107,6 +108,15 @@ export default function RootLayout({
           </NotificationRoot>
         </Providers>
         <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID || ""} />
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "vpco7b57pc");
+          `}
+        </Script>
       </body>
     </html>
   );
